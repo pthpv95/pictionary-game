1e38e1ad7c0b49cf7ed5bf72ffbb79cd888eefe2
Hien Pham <thehienpv@gmail.com>
2020-04-27 00:57:21 +0700

    use express server

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-const app = require('http').createServer(handler)
-const io = require('socket.io')(app);
-const fs = require('fs');
+const path = require("path");
+const publicPath = path.join(__dirname, "./public");
+const express = require("express");
+const socketIO = require("socket.io");
 
-app.listen(8080, ()=>{
-  console.log('Pictionary app is up and running..');
+const app = express();
+var port = process.env.PORT || 8080;
+
+const http = require("http");
+var server = http.createServer(app);
+var io = socketIO(server, {
+  pingTimeout: 60000
 });
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  (err, data) => {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
 
 io.on('connection', (socket) => {
   socket.emit('news', { hello: 'world' });
@@ -28,4 +21,9 @@ io.on('connection', (socket) => {
   socket.on('canvas', (data) => {
     io.emit('canvas-positions', data);
   });
+});
+
+app.use(express.static(publicPath));
+server.listen(port, () => {
+  console.log(`Running on http://localhost:${port}`);
 });
